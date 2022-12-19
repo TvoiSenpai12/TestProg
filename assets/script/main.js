395a5c9de6c8d3caeac92edbedcf2b225bc01053
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-12-19 03:08:22 +0300

    Added appearance and hiding for popop

---
+++ assets/script/main.js
@@ -0,0 +1,3 @@
+document.querySelector('.select_gorod').onclick = () => {document.querySelector('.popup').classList.remove('hide');}
+
+document.querySelector('.close').onclick = () => {document.querySelector('.popup').classList.add('hide');}
